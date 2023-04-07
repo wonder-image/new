d0f13da7e1ad8cf9e8108dc668e5aa36ce213764
Andrea Marinoni <marinoni@wonderimage.it>
2023-04-07 15:33:53 +0200

    Update permissions.php

--- site_name/custom/config/permissions.php
+++ site_name/custom/config/permissions.php
@@ -1,28 +1,6 @@
 <?php
 
     // Authority
-    $CUSTOM_PERMITS = [
-        "frontend" => [
-            "commercial" => [
-                "name" => "Commerciale",
-                "icon" => "<i class='bi bi-cash-stack'></i>",
-                "bg" => "bg-success",
-                "tx" => "text-white",
-                "color" => "success",
-                "links" => [
-                    "login" => "$PATH->site/preventive/account/login/",
-                    "sign-in" => "",
-                    "password-restore" => "$PATH->site/preventive/account/password-restore/",
-                    "password-recovery" => "$PATH->site/preventive/account/password-recovery/",
-                    "password-set" => "$PATH->site/preventive/account/password-set/"
-                ],
-                "function" => [
-                    "creation" => "commercial",
-                    "modify" => "commercial",
-                    "info" => "infoCommercial"
-                ]
-            ]
-        ]
-    ];
+    $CUSTOM_PERMITS = [];
 
 ?>
