d90cab3b945d1ad1533dbf5d358ced8c216358b6
Andrea Marinoni <marinoni@wonderimage.it>
2023-12-29 10:11:42 +0100

    Nuovo metodo di aggiornamento

--- site_name/update/index.php
+++ site_name/update/index.php
@@ -7,9 +7,6 @@
 
     $ROOT = $_SERVER['DOCUMENT_ROOT'];
     require_once $ROOT."/vendor/wonder-image/app/wonder-image.php";
+    require_once $ROOT_APP."/html/default/update.php";
 
-    require_once $ROOT."/update/table/index.php";
-    require_once $ROOT."/update/row/index.php";
-    require_once $ROOT."/update/page/index.php";
-    
 ?>
